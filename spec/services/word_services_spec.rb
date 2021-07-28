e15e67d2b856f68df647b4031dc71f70662ae7dd
Alex Ferencz <alex.ferencz6@gmail.com>
2021-07-28 15:25:43 -0600

    Word search service and test created

---
+++ spec/services/word_services_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'word services' do
+  it 'can find english(US) words' do
+    VCR.use_cassette 'word search' do
+      result = WordService.search('caterwaul')
+      expect(result.first[:word]).to eq('caterwaul')
+      expect(result.first).to have_key(:phonetics)
+      expect(result.first).to have_key(:meanings)
+    end
+  end
+end
