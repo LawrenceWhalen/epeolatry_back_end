1fde13d2cff235ca7841ea03235b851569da3dac
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-27 22:02:08 -0600

    Setup travic heroku pipline

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 0) do
+=======
+ActiveRecord::Schema.define(version: 2021_07_28_040022) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+<<<<<<< Updated upstream
+=======
+  create_table "words", force: :cascade do |t|
+    t.string "word"
+    t.string "definition"
+    t.string "example"
+    t.string "part_of_speech"
+    t.string "synonyms"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+>>>>>>> Stashed changes
 end
