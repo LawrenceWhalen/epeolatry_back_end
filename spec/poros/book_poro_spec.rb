0d524809a151704fe9bb11e41ab573cd36c4afb3
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-28 16:36:25 -0600

    Creat book poro tests

--- spec/poros/book_poro_spec.rb
+++ spec/poros/book_poro_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'book poro' do
-  xit 'has attributes' do
-    not_harry_potter = Book.new(title: 'stinky cheese man')
+RSpec.describe Book do
+  it 'has attributes' do
+    actual = Book.new(title: 'stinky cheese man', authors: ['Alex Smell', 'Wanda Feet'], description: 'A breathtaking book', genres: ['non-fiction'])
+
+    expect(actual.title).to eq('stinky cheese man')
+    expect(actual.authors).to eq(['Alex Smell', 'Wanda Feet'])
+    expect(actual.description).to eq('A breathtaking book')
+    expect(actual.genres).to eq(['non-fiction'])
   end
 end
