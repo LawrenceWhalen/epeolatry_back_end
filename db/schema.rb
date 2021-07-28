c8e569ec0e4d88daee406675890824feb421672f
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-27 22:06:11 -0600

    Clean schema migration conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 2021_07_28_040022) do
->>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< Updated upstream
-=======
   create_table "words", force: :cascade do |t|
     t.string "word"
     t.string "definition"
@@ -32,5 +26,4 @@ ActiveRecord::Schema.define(version: 2021_07_28_040022) do
     t.datetime "updated_at", null: false
   end
 
->>>>>>> Stashed changes
 end
