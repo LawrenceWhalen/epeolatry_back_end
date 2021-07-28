91eff2dbbb0adb71c541146c9a220740c38d1c8c
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-28 16:58:25 -0600

    Create migration to remove books table

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,3 +1,14 @@
 class Book
+  attr_reader :title, 
+              :authors, 
+              :description, 
+              :genres
+
+  def initialize(attributes)
+    @title = attributes[:title]
+    @authors = attributes[:authors]
+    @genres = attributes[:genres]
+    @description = attributes[:description]
+  end
 
 end
