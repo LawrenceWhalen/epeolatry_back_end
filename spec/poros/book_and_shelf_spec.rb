3de2f62e80a34f03c15f7ebd413f25366324d93c
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-29 14:52:05 -0600

    Create test for book and shelf poro

---
+++ spec/poros/book_and_shelf_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Book do
+  it 'has attributes' do
+    actual = BookPoro.new(g_id: '123', 
+                          title: 'stinky cheese man', 
+                          authors: ['Alex Smell', 'Wanda Feet'], 
+                          description: 'A breathtaking book', 
+                          genres: ['non-fiction'], 
+                          shelves: ['Reading now', 'Have read'])
+
+    expect(actual.g_id).to eq('123')
+    expect(actual.title).to eq('stinky cheese man')
+    expect(actual.authors).to eq(['Alex Smell', 'Wanda Feet'])
+    expect(actual.description).to eq('A breathtaking book')
+    expect(actual.genres).to eq(['non-fiction'])
+    expect(actual.shelves).to eq(['Reading now', 'Have read'])
+  end
+end
