d21e6788cbda6bcda39214bc0cf2dada302b0d62
Alex Ferencz <alex.ferencz6@gmail.com>
2021-07-29 16:07:30 -0600

    Fix testing and add return map

--- spec/requests/search_words_spec.rb
+++ spec/requests/search_words_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'get: /words/search' do
   describe 'requesting a search' do
     it 'returns quested info' do
-      VCR.use_cassette 'word search' do
-      get "/words/search/caterwaul"
+      VCR.use_cassette 'definition search' do
+      get "/words/search?word=caterwaul"
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(result[:data][:attributes][:word]).to eq('caterwaul')
