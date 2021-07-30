3a12a1c515060f881888768081602153254aee8a
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-30 09:53:10 -0600

    Add a returns map for user/books

---
+++ return_maps/user_books_return.rb
@@ -0,0 +1,48 @@
+get: 'https://epeolatry-back-end.herokuapp.com/user/books' 
+    params: { auth_token: 'the users auth_token from google' }
+
+[{:id=>"PCcOMbEydAIC",
+  :type=>"book_and_shelf",
+  :attributes=>
+   {:title=>"Lilith's Brood",
+    :authors=>["Octavia E. Butler"],
+    :genres=>["Fiction"],
+    :description=>
+     "The complete series about an alien species that could save humanity after nuclear apocalypse—or destroy it—from “one of science fiction’s finest writers” (The New York Times). The newest stage in human evolution begins in outer space. Survivors ...",
+    :shelves=>["Reading now"]}},
+ {:id=>"ZrNzAwAAQBAJ",
+  :type=>"book_and_shelf",
+  :attributes=>
+   {:title=>"The Three-Body Problem",
+    :authors=>["Cixin Liu"],
+    :genres=>["Fiction"],
+    :description=>
+     "Soon to be a Netflix Original Series! “War of the Worlds for the 21st century.” – Wall Street Journal The Three-Body Problem is the first chance for English-speaking readers to experience the Hugo Award-winning phenomenon from China's most ...",
+    :shelves=>["Reading now"]}},
+ {:id=>"8thMLkahggcC",
+  :type=>"book_and_shelf",
+  :attributes=>
+   {:title=>"Parable of the Sower",
+    :authors=>["Octavia E. Butler"],
+    :genres=>["Fiction"],
+    :description=>
+     "A New York Times Notable Book: In 2025, with the world descending into madness and anarchy, one woman begins a fateful journey toward a better future. “A stunner.” —Flea, musician and actor, TheWall Street Journal Lauren Olamina and her family ...",
+    :shelves=>["Reading now", "To read"]}},
+ {:id=>"yrYUvgAACAAJ",
+  :type=>"book_and_shelf",
+  :attributes=>
+   {:title=>"All You Need Is Kill",
+    :authors=>["Hiroshi Sakurazaka"],
+    :genres=>["Fiction"],
+    :description=>
+     "When the alien Mimics invade, Keiji Kiriya is just one of many recruits shoved into a suit of battle armor called a Jacket and sent out to kill. Keiji dies on the battlefield, only to be reborn each morning to fight and die again and again. On ...",
+    :shelves=>["Reading now"]}},
+ {:id=>"inYs79gV4UQC",
+  :type=>"book_and_shelf",
+  :attributes=>
+   {:title=>"Snow Crash",
+    :authors=>["Neal Stephenson"],
+    :genres=>["Fiction"],
+    :description=>
+     "\"This Snow Crash thing--is it a virus, a drug, or a religion?\" Juanita shrugs. \"What's the difference?\" The only relief from the sea of logos is within the well-guarded borders of the Burbclaves. Is it any wonder that most sane folks have ...",
+    :shelves=>["To read"]}}]
