d43613e92c4762033a76c8ac7f2aa080e6510158
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-31 16:14:22 -0700

    Add note to remember change needed, add destroy method

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,12 +1,17 @@
 class UserBooksController < ApplicationController
 
   def index
-    books = UserBooksFacade.all_books(params[:auth_token])
+    books = UserBooksFacade.all_books(params[:auth_token])   # needs a user id and a filter_by(can be default nothing) in the params
     render json: BookAndShelfSerializer.new(books)
+    # pagination here will be difficult, going to try and forgo it for the moment
+    # needs to add in a conditional in order to ensure that is sorted correctly,
   end
 
   def create
     UserBooksFacade.add_book(params[:volume_id], params[:auth_token])
   end
 
+  def destroy
+    BookService.
+  end
 end
