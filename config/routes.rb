fa393261fe9d6492cbeb78b6c7c52328cb5f5d45
Aliya Merali <merali4@gmail.com>
2021-07-30 19:29:28 -0600

    Add request spec for create book action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/books/search', to: 'book_search#index'
   get '/user/books', to: 'user_books#index'
   get '/words/search', to: 'words#search'
+
+  # post '/user/books', to: 'user_books#create'
+  # delete '/user/books/:id', to: 'user_books#destroy'
 end
