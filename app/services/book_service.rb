f9c8c14ffb57dd981df904b94dc394ba958782a0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-31 18:00:11 -0700

    Added shelf_id argument, commented out finding shelf id method, possibly unneeded

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -27,12 +27,11 @@ class BookService
     JSON.parse(body, symbolize_names: true)
   end
 
-  def self.add_book(volume_id, auth_token)
-    shelf =  BookService.book_shelves(auth_token)[:items].find do |shelf|
-                shelf[:title] == "To read"
-              end
-    shelf_id = shelf[:id]
-
+  def self.add_book(shelf_id, volume_id, auth_token)
+    # shelf =  BookService.book_shelves(auth_token)[:items].find do |shelf|
+    #             shelf[:title] == "To read"
+    #           end
+    # shelf_id = shelf[:id]
     conn(auth_token).get "/books/v1/mylibrary/bookshelves/#{shelf_id}/addVolume?volumeId=#{volume_id}"
   end
 
