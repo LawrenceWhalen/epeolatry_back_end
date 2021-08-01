615542144bb1404c777efa8f1a6bcfbeb98eb303
Aliya Merali <merali4@gmail.com>
2021-08-01 15:02:54 -0600

    Add test for userwordsfacade

---
+++ app/facades/user_words_facade.rb
@@ -0,0 +1,4 @@
+class UserWordFacade
+
+  
+end
---
+++ spec/facades/user_words_facade_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'user words facade' do
+  describe 'class methods' do
+    describe '.create_word' do
+      before :each do
+        word_content = 'test'
+        response_body = File.read('spec/fixtures/word_search.json')
+        stub_request(:get, "https://api.dictionaryapi.dev/api/v2/entries/en_US/#{word_content}").
+          with(
+            headers: {
+                     'Accept'=>'*/*',
+                     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                     'User-Agent'=>'Faraday v1.5.1'
+                      }).
+          to_return(status: 200, body: response_body, headers: {})
+
+          non_word = 'gte'
+          response_body2 = File.read('spec/fixtures/word_search_fail.json')
+          stub_request(:get, "https://api.dictionaryapi.dev/api/v2/entries/en_US/#{non_word}").
+            with(
+              headers: {
+                       'Accept'=>'*/*',
+                       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                       'User-Agent'=>'Faraday v1.5.1'
+                        }).
+            to_return(status: 200, body: response_body2, headers: {})
+      end
+
+      it 'looks up a word and returns a word object with response' do
+        input_word = 'test'
+
+        UserWordsFacade.create_word(input_word)
+
+        new_word = Word.last
+
+        expect(new_word.word).to eq(input_word)
+        expect(new_word.definition).to eq("Take measures to check the quality, performance, or reliability of (something), especially before putting it into widespread use or practice.")
+        expect(new_word.phonetic).to eq("/tɛst/")
+        expect(new_word.phonetic_link).to eq("https://lex-audio.useremarkable.com/mp3/test_us_1.mp3")
+        expect(new_word.part_of_speech).to eq("transitive verb")
+        expect(new_word.example).to eq("this range has not been tested on animals")
+        expect(new_word.synonyms.length).to eq(7)
+        expect(new_word.synonyms.first).to eq("try out")
+      end
+
+      it 'does not add word if word not found' do
+        input_word = 'test'
+
+        output = UserWordsFacade.create_word(input_word)
+
+        expect(Word.last.word).to_not eq(input_word)
+      end
+    end
+  end
+end
