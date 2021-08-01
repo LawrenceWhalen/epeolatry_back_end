b30e57d2dfc334b21bf153500a625b7ebb14074d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-01 11:14:28 -0700

    Add in namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/books/search', to: 'book_search#index'
-
-  get '/words/search', to: 'words#search'
-
-  get '/user/books', to: 'user_books#index'
-  post '/user/books', to: 'user_books#create'
-  delete '/user/books/:id', to: 'user_books#destroy'
 
   namespace :api do
     namespace :v1 do
+      get '/book/search', to: 'book_search#index'
+      get '/word/search', to: 'word_search#search'
       namespace :user do
+        resources :books, only: [:index, :create, :destroy]
         resources :words, only: [:index]
       end
     end
