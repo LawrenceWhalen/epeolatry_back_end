209008b503e5e1bbd6deaf8a7aa7c8fb2d1e72e2
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-31 19:54:20 -0700

    Add namespace for api, version and user, add words resources index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/books/search', to: 'book_search#index'
-  get '/user/books', to: 'user_books#index'
+
   get '/words/search', to: 'words#search'
 
+  get '/user/books', to: 'user_books#index'
   post '/user/books', to: 'user_books#create'
   delete '/user/books/:id', to: 'user_books#destroy'
+
+  namespace :api do
+    namespace :v1 do
+      namespace :user do
+        resources :words, only: [:index]
+      end
+    end
+  end
 end
