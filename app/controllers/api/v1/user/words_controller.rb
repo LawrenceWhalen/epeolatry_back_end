bc214d1a4257a0b5bb18e7050c7dcf78a07a5427
Aliya Merali <merali4@gmail.com>
2021-08-01 14:47:16 -0600

    Add basic functionality for add word to glossary endpoint

--- app/controllers/api/v1/user/words_controller.rb
+++ app/controllers/api/v1/user/words_controller.rb
@@ -12,7 +12,16 @@ class Api::V1::User::WordsController < ApplicationController
   end
 
   def create
-
+    word_search_response = WordService.search(params[:word])
+    new_word = Word.create(word: word_search_response.first[:word],
+                           definition: word_search_response.first[:meanings].first[:definitions].first[:definition],
+                           phonetic: word_search_response.first[:phonetics].first[:text],
+                           phonetic_link: word_search_response.first[:phonetics].first[:audio],
+                           part_of_speech: word_search_response.first[:meanings].first[:partOfSpeech],
+                           synonyms: word_search_response.first[:meanings].first[:definitions].first[:synonyms],
+                           example: word_search_response.first[:meanings].first[:definitions].first[:example])
+    Glossary.create(word_id: new_word.id, book_id: params[:volume_id], user_id: params[:user_id])
+    render json: { response: 'Created' }, status: :created
   end
 
   def edit
--- spec/requests/api/v1/user/words_request_spec.rb
+++ spec/requests/api/v1/user/words_request_spec.rb
@@ -10,16 +10,19 @@ RSpec.describe 'Words Request' do
   end
 
   it 'can add a word to a user/book combo' do
+    word_content = 'test'
+    response_body = File.read('spec/fixtures/word_search.json')
+    stub_request(:get, "https://api.dictionaryapi.dev/api/v2/entries/en_US/#{word_content}").
+      with(
+        headers: {
+                 'Accept'=>'*/*',
+                 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                 'User-Agent'=>'Faraday v1.5.1'
+                  }).
+      to_return(status: 200, body: response_body, headers: {})
+
     user_id = 1
     book_id = 'ju23gs48g'
-    # new_word = Word.create!(word: "test",
-    #                     definition: "an sample item used for testing",
-    #                     example: "this is a test",
-    #                     part_of_speech: "noun",
-    #                     synonyms: ["example", "sample"],
-    #                     phonetic: 't-eeee-zt',
-    #                     phonetic_link: 'www.google.com')
-    word_content = 'test'
     headers = {"CONTENT_TYPE" => "application/json"}
 
     post '/api/v1/user/words', headers: headers, params: JSON.generate(word: word_content, volume_id: book_id, user_id: user_id)
@@ -28,6 +31,6 @@ RSpec.describe 'Words Request' do
 
     expect(response).to be_successful
     expect(new_word.word).to eq(word_content)
-    expect(Glossary.where('word_id = ?, book_id = ?, user_id = ?', new_word.id, book_id, user_id).exists?).to eq(true)
+    expect(Glossary.where('word_id = ? AND book_id = ? AND user_id = ?', new_word.id, book_id, user_id).exists?).to eq(true)
   end
 end
