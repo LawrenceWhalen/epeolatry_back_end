70bb6a2dc0a35244cf7cd8d30ca185d7e1f17037
Aliya Merali <merali4@gmail.com>
2021-08-01 18:42:03 -0600

    Add return map for endpoint

---
+++ return_maps/user_words_index_return.rb
@@ -0,0 +1,21 @@
+get: 'https://epeolatry-back-end.herokuapp.com/api/v1/user/words'
+    params: { user_id: user_id }
+
+{:data=>
+[
+  {:id=>"183", :type=>"word", :attributes=>{:word=>"fvpiggiiq0", :definition=>"Maxime hic omnis facere.", :phonetic=>"zllmo", :phonetic_link=>"0i623sqdhdi63fq", :part_of_speech=>"r37bddcbpe", :synonyms=>"owk8h3f78h", :example=>"Possimus quam veniam autem."}},
+  {:id=>"184", :type=>"word", :attributes=>{:word=>"nplzwe0m9p", :definition=>"Animi rerum suscipit atque.", :phonetic=>"lwow2", :phonetic_link=>"xkdhjqwkmg7q96e", :part_of_speech=>"xvny6vidcu", :synonyms=>"gw5new4mls", :example=>"Dolores maiores quisquam quia."}},
+  {:id=>"185", :type=>"word", :attributes=>{:word=>"xfkn4551ub", :definition=>"Ab praesentium non qui.", :phonetic=>"xdze6", :phonetic_link=>"k5yhpyrb2agatd2", :part_of_speech=>"6y4yl75xvp", :synonyms=>"q7nte1z12f", :example=>"Qui sed perspiciatis dolore."}},
+  {:id=>"186",
+  :type=>"word",
+  :attributes=>{:word=>"6hq1vxpuo4", :definition=>"Voluptates expedita dolorem est.", :phonetic=>"meb64", :phonetic_link=>"1ykboq3bukwnsw5", :part_of_speech=>"lkbiphs2dz", :synonyms=>"bntjkhkecb", :example=>"Eligendi corrupti ut atque."}},
+ {:id=>"187",
+  :type=>"word",
+  :attributes=>{:word=>"15r4gzuik7", :definition=>"Non ipsa repellendus et.", :phonetic=>"du6ko", :phonetic_link=>"o4rrnnfzb6nvz45", :part_of_speech=>"lycpblhhvp", :synonyms=>"64fyvxy99v", :example=>"Sed molestias laudantium cupiditate."}},
+ {:id=>"188", :type=>"word", :attributes=>{:word=>"4lx0cfxwc8", :definition=>"Quam ipsum autem mollitia.", :phonetic=>"99jxu", :phonetic_link=>"hiz1w8juv2rmue2", :part_of_speech=>"0w3orjcuum", :synonyms=>"3s7nzfvh7f", :example=>"Modi asperiores qui facilis."}},
+ {:id=>"189", :type=>"word", :attributes=>{:word=>"46x7x32dsq", :definition=>"Corrupti nobis eum maiores.", :phonetic=>"06wnc", :phonetic_link=>"fgqge74uk99i43v", :part_of_speech=>"dw0phr5fye", :synonyms=>"d0suj1kjeq", :example=>"Et deleniti quia et."}},
+ {:id=>"190", :type=>"word", :attributes=>{:word=>"35mldkyf2o", :definition=>"Occaecati ad non qui.", :phonetic=>"f0r8u", :phonetic_link=>"cf84tn8ter4py1j", :part_of_speech=>"sp1lx45c2x", :synonyms=>"lyblikpdb9", :example=>"Consequatur dolore quod a."}},
+ {:id=>"191", :type=>"word", :attributes=>{:word=>"2sa94n1pcu", :definition=>"Natus quisquam voluptas delectus.", :phonetic=>"f5bmk", :phonetic_link=>"0ny8f624ys0jopc", :part_of_speech=>"19i8heqiti", :synonyms=>"w8ycizb41z", :example=>"Veritatis ipsum autem fugit."}
+ }
+ ]
+}
