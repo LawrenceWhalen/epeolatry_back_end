90d413e9ba77733b93d755ccf08694246db1669d
Aliya Merali <merali4@gmail.com>
2021-08-05 16:31:21 -0600

    Refactor .all books to eliminate additional API call

--- app/facades/user_books_facade.rb
+++ app/facades/user_books_facade.rb
@@ -1,35 +1,38 @@
 class UserBooksFacade
 
   def self.all_books(auth_token)
-    shelves = BookService.book_shelves(auth_token)
+    shelf_ids = {3 =>"Reading now", 2=>"To read", 4=>"Have read"}
 
-    shelf_ids = shelves[:items].map do |shelf|
-      if shelf[:title] == "Reading now" || shelf[:title] == "To read" || shelf[:title] == "Have read"
-        { id: shelf[:id], title: shelf[:title] }
+    all_book_ids = []
+    all_books = []
+
+    shelf_ids.keys.each do |shelf_id|
+      shelf_contents = BookService.books_on_shelf(shelf_id, auth_token)[:items]
+
+      if !shelf_contents.nil?
+        shelf_contents.map do |book|
+          if all_book_ids.include?(book[:id])
+            added_book = all_books.find do |added_book|
+              added_book.g_id == book[:id]
+            end
+            added_book.shelves << shelf_ids[shelf_id]
+          else
+            book = BookAndShelf.new(
+              g_id: book[:id],
+              title: book[:volumeInfo][:title],
+              authors: book[:volumeInfo][:authors],
+              description: book[:volumeInfo][:description],
+              genres: book[:volumeInfo][:categories],
+              shelves: [shelf_ids[shelf_id]]
+            )
+            all_book_ids << book.g_id
+            all_books << book
+          end
+        end
       end
-    end.compact
+    end
 
-    all_books = shelf_ids.map do |shelf|
-      contents = BookService.books_on_shelf(shelf[:id], auth_token)[:items]
-      contents.map do |book|
-        BookAndShelf.new(
-          g_id: book[:id],
-          title: book[:volumeInfo][:title],
-          authors: book[:volumeInfo][:authors],
-          description: book[:volumeInfo][:description],
-          genres: book[:volumeInfo][:categories],
-          shelves: [shelf[:title]]
-        )
-      end if contents
-    end.flatten
-
-    all_books.compact.each_with_object({}) do |book, hash|
-      if hash[book.g_id]
-        hash[book.g_id].shelves += book.shelves
-      else
-        hash[book.g_id] = book
-      end
-    end.values
+    all_books
   end
 
   def self.single_book(auth_token, book_id)
