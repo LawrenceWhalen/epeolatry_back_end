3f0b7ba53249d7b329966c11cbb996149ca91e4b
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-08-05 17:59:39 -0600

    Refactor serializer.

--- app/serializers/dashboard_serializer.rb
+++ app/serializers/dashboard_serializer.rb
@@ -1,10 +1,17 @@
 class DashboardSerializer
 
   def self.word_stats(word_stats)
-    {
-      avg_per_book: word_stats[:avg_per_book],
-      most_frequent: word_stats[:most_frequent],
-      longest_word: word_stats[:longest_word]
+    {data:
+      { id: nil,
+        type: 'word stats',
+        attributes: {
+          avg_per_book: word_stats[:avg_per_book],
+          most_frequent: word_stats[:most_frequent],
+          longest_word: word_stats[:longest_word]
+        }
+
+      }
+
     }
   end
 end
